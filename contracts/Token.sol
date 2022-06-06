0f9bb14f2949bc3694953a2592258e45c60a9258
gstefanoni <gstefanoni@studenti.uninsibria.it>
2022-06-06 12:22:14 +0200

    Aggiornato token

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,19 +6,33 @@ import "hardhat/console.sol";
 
 // This is the main building block for smart contracts.
 contract Token {
+     /**
+     * Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
     // Some string type variables to identify the token.
     // The `public` modifier makes a variable readable from outside the contract.
-    string public name = "GianlucaToken";
-    string public symbol = "GLT";
+    string public _name = "GianlucaToken";
+    string public _symbol = "GLT";
 
     // The fixed amount of tokens stored in an unsigned integer type variable.
-    uint256 public totalSupply = 1000000;
+    uint256 public _totalSupply = 1000000;
 
     // An address type variable is used to store ethereum accounts.
-    address public owner;
+    address public _owner;
 
     // A mapping is a key/value map. Here we store each account balance.
-    mapping(address => uint256) balances;
+    mapping(address => uint256) _balances;
 
     
     /**
@@ -29,8 +43,45 @@ contract Token {
     constructor() {
         // The totalSupply is assigned to transaction sender, which is the account
         // that is deploying the contract.
-        balances[msg.sender] = totalSupply;
-        owner = msg.sender;
+        _balances[msg.sender] = _totalSupply;
+        _owner = msg.sender;
+    }
+
+     /**
+     *Returns the name of the token.
+     */
+    function name() public view virtual returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
+    function symbol() public view virtual returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei.
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract.
+     */
+    function decimals() public view virtual returns (uint8) {
+        return 1;
+    }
+
+     /**
+     * Returns the total supply amount
+     */
+    function totalSupply() public view virtual  returns (uint256) {
+        return _totalSupply;
     }
 
     /**
@@ -40,17 +91,42 @@ contract Token {
      * the contract.
      */
     function transfer(address to, uint256 amount) external {
-        console.log("Sender balance is %s tokens", balances[msg.sender]);
+        _beforeTokenTransfer(msg.sender, to, amount);
+        console.log("Sender balance is %s tokens", _balances[msg.sender]);
         console.log("Trying to send %s tokens to %s", amount, to);
 
         // Check if the transaction sender has enough tokens.
         // If `require`'s first argument evaluates to `false` then the
         // transaction will revert.
-        require(balances[msg.sender] >= amount, "Not enough tokens");
+        require(_balances[msg.sender] >= amount, "Not enough tokens");
 
         // Transfer the amount.
-        balances[msg.sender] -= amount;
-        balances[to] += amount;
+        _balances[msg.sender] -= amount;
+        _balances[to] += amount;
+
+        _afterTokenTransfer(msg.sender, to, amount);
+    }
+
+     /**
+     * A function to transfer tokens, from an address to an other.
+     *
+     * The `external` modifier makes a function *only* callable from outside
+     * the contract.
+     */
+    function transferFrom(address _from, address _to, uint256 amount) external {
+        _beforeTokenTransfer(_from, _to, amount);
+        console.log("Sender balance is %s tokens", _balances[_from]);
+        console.log("Trying to send %s tokens to %s", amount, _to);
+
+        // Check if the transaction sender has enough tokens.
+        // If `require`'s first argument evaluates to `false` then the
+        // transaction will revert.
+        require(_balances[_from] >= amount, "Not enough tokens");
+
+        // Transfer the amount.
+        _balances[_from] -= amount;
+        _balances[_to] += amount;
+        _afterTokenTransfer(_from, _to, amount);
     }
 
     /**
@@ -60,6 +136,95 @@ contract Token {
      * state, which allows us to call it without executing a transaction.
      */
     function balanceOf(address account) external view returns (uint256) {
-        return balances[account];
+        return _balances[account];
     }
+
+    /** Creates `amount` tokens and assigns them to `account`, increasing
+     * the total supply.
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     */
+    function _mint(address account, uint256 amount) internal virtual {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _beforeTokenTransfer(address(0), account, amount);
+
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+
+        _afterTokenTransfer(address(0), account, amount);
+    }
+
+    /**
+     * Destroys `amount` tokens from `account`, reducing the
+     * total supply.
+     *
+     * Emits a {Transfer} event with `to` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     * - `account` must have at least `amount` tokens.
+     */
+    function _burn(address account, uint256 amount) internal virtual {
+        require(account != address(0), "ERC20: burn from the zero address");
+
+        _beforeTokenTransfer(account, address(0), amount);
+
+        uint256 accountBalance = _balances[account];
+        require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
+        unchecked {
+            _balances[account] = accountBalance - amount;
+        }
+        _totalSupply -= amount;
+
+        emit Transfer(account, address(0), amount);
+
+        _afterTokenTransfer(account, address(0), amount);
+    }
+
+    /**
+     *  Hook that is called before any transfer of tokens. This includes
+     * minting and burning.
+     *
+     * Calling conditions:
+     *
+     * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
+     * will be transferred to `to`.
+     * - when `from` is zero, `amount` tokens will be minted for `to`.
+     * - when `to` is zero, `amount` of ``from``'s tokens will be burned.
+     * - `from` and `to` are never both zero.
+     *
+     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+     */
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual {}
+
+    /**
+     * Hook that is called after any transfer of tokens. This includes
+     * minting and burning.
+     *
+     * Calling conditions:
+     *
+     * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
+     * has been transferred to `to`.
+     * - when `from` is zero, `amount` tokens have been minted for `to`.
+     * - when `to` is zero, `amount` of ``from``'s tokens have been burned.
+     * - `from` and `to` are never both zero.
+     *
+     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+     */
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual {}
 }
