09357816f55b77af3dc5497a7a61f50d7d4ee370
gstefanoni <gstefanoni@studenti.uninsibria.it>
2022-06-07 14:50:33 +0200

    Standardizzato token con ERC20 di OpenZeppelin

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -34,6 +34,8 @@ contract Token {
     // A mapping is a key/value map. Here we store each account balance.
     mapping(address => uint256) _balances;
 
+    mapping(address => mapping(address => uint256)) private _allowances;
+
     
     /**
      * Contract initialization.
@@ -90,21 +92,9 @@ contract Token {
      * The `external` modifier makes a function *only* callable from outside
      * the contract.
      */
-    function transfer(address to, uint256 amount) external {
-        _beforeTokenTransfer(msg.sender, to, amount);
-        console.log("Sender balance is %s tokens", _balances[msg.sender]);
-        console.log("Trying to send %s tokens to %s", amount, to);
-
-        // Check if the transaction sender has enough tokens.
-        // If `require`'s first argument evaluates to `false` then the
-        // transaction will revert.
-        require(_balances[msg.sender] >= amount, "Not enough tokens");
-
-        // Transfer the amount.
-        _balances[msg.sender] -= amount;
-        _balances[to] += amount;
-
-        _afterTokenTransfer(msg.sender, to, amount);
+    function transfer(address to, uint256 amount) public virtual returns (bool) {
+        _transfer(_owner, to, amount);
+        return true;
     }
 
      /**
@@ -113,20 +103,21 @@ contract Token {
      * The `external` modifier makes a function *only* callable from outside
      * the contract.
      */
-    function transferFrom(address _from, address _to, uint256 amount) external {
-        _beforeTokenTransfer(_from, _to, amount);
-        console.log("Sender balance is %s tokens", _balances[_from]);
-        console.log("Trying to send %s tokens to %s", amount, _to);
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public virtual returns (bool) {
+        _spendAllowance(from, _owner, amount);
+        _transfer(from, to, amount);
+        return true;
+    }
 
-        // Check if the transaction sender has enough tokens.
-        // If `require`'s first argument evaluates to `false` then the
-        // transaction will revert.
-        require(_balances[_from] >= amount, "Not enough tokens");
-
-        // Transfer the amount.
-        _balances[_from] -= amount;
-        _balances[_to] += amount;
-        _afterTokenTransfer(_from, _to, amount);
+     /**
+     * @dev See {IERC20-allowance}.
+     */
+    function allowance(address owner, address spender) public view virtual returns (uint256) {
+        return _allowances[owner][spender];
     }
 
     /**
@@ -227,4 +218,143 @@ contract Token {
         address to,
         uint256 amount
     ) internal virtual {}
+
+     /**
+     *
+     * NOTE: If `amount` is the maximum `uint256`, the allowance is not updated on
+     * `transferFrom`. This is semantically equivalent to an infinite approval.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     */
+    function approve(address spender, uint256 amount) public virtual returns (bool) {
+        _approve(_owner, spender, amount);
+        return true;
+    }
+
+    
+    /**
+     * Atomically increases the allowance granted to `spender` by the caller.
+     *
+     * This is an alternative to {approve} that can be used as a mitigation for
+     * problems described in {IERC20-approve}.
+     *
+     * Emits an {Approval} event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     */
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+        _approve(_owner, spender, allowance(_owner, spender) + addedValue);
+        return true;
+    }
+
+    /**
+     * Atomically decreases the allowance granted to `spender` by the caller.
+     *
+     * This is an alternative to {approve} that can be used as a mitigation for
+     * problems described in {IERC20-approve}.
+     *
+     * Emits an {Approval} event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     * - `spender` must have allowance for the caller of at least
+     * `subtractedValue`.
+     */
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+        uint256 currentAllowance = allowance(_owner, spender);
+        require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
+        unchecked {
+            _approve(_owner, spender, currentAllowance - subtractedValue);
+        }
+
+        return true;
+    }
+
+    /**
+     * Moves `amount` of tokens from `from` to `to`.
+     *
+     * This internal function is equivalent to {transfer}, and can be used to
+     * e.g. implement automatic token fees, slashing mechanisms, etc.
+     *
+     * Emits a {Transfer} event.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `from` must have a balance of at least `amount`.
+     */
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual {
+        require(from != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0), "ERC20: transfer to the zero address");
+
+        _beforeTokenTransfer(from, to, amount);
+
+        uint256 fromBalance = _balances[from];
+        require(fromBalance >= amount, "ERC20: transfer amount exceeds balance");
+        unchecked {
+            _balances[from] = fromBalance - amount;
+        }
+        _balances[to] += amount;
+
+        emit Transfer(from, to, amount);
+
+        _afterTokenTransfer(from, to, amount);
+    }
+
+    /**
+     * Sets `amount` as the allowance of `spender` over the `owner` s tokens.
+     *
+     * This internal function is equivalent to `approve`, and can be used to
+     * e.g. set automatic allowances for certain subsystems, etc.
+     *
+     * Emits an {Approval} event.
+     *
+     * Requirements:
+     *
+     * - `owner` cannot be the zero address.
+     * - `spender` cannot be the zero address.
+     */
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal virtual {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+     /**
+     * @dev Updates `owner` s allowance for `spender` based on spent `amount`.
+     *
+     * Does not update the allowance amount in case of infinite allowance.
+     * Revert if not enough allowance is available.
+     *
+     * Might emit an {Approval} event.
+     */
+    function _spendAllowance(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal virtual {
+        uint256 currentAllowance = allowance(owner, spender);
+        if (currentAllowance != type(uint256).max) {
+            require(currentAllowance >= amount, "ERC20: insufficient allowance");
+            unchecked {
+                _approve(owner, spender, currentAllowance - amount);
+            }
+        }
+    }
 }
